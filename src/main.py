7bca87eefb259096828194946ee38b157ba6ccf4
gbrunini <german.brunini@machineslikeme.com>
2024-12-09 08:29:52 -0300

    deleted ruff.toml file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from random import randrange
 from typing import Optional
 
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, HTTPException, status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,53 +15,56 @@ class Post(BaseModel):
 
 
 my_post = [
-    {"title": "this title 1", "content": "this content 1", "id": 1},
-    {"title": "this title 2", "content": "this content 2", "id": 2},
+    {'title': 'this title 1', 'content': 'this content 1', 'id': 1},
+    {'title': 'this title 2', 'content': 'this content 2', 'id': 2},
 ]
 
 
 def hello():
-    print("Hello world!!")
+    print('Hello world!!')
 
 
 def find_post(id: int):
     for p in my_post:
-        if p["id"] == id:
+        if p['id'] == id:
             return p
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return "Hello world!!!"
+    return 'Hello world!!!'
 
 
-@app.get("/greet/{name}")
+@app.get('/greet/{name}')
 async def greet_name(name: str, age: int) -> dict:
-    return {"message": f"Hello {name}!!!", "age": age}
+    return {'message': f'Hello {name}!!!', 'age': age}
 
 
-@app.get("/posts")
+@app.get('/posts')
 async def get_post():
-    return {"data": my_post}
+    return {'data': my_post}
 
 
-@app.get("/posts/latest")
+@app.get('/posts/latest')
 async def get_latest_post():
     post = my_post[len(my_post) - 1]
-    return {"latest_post": f"here is your latest {post}"}
+    return {'latest_post': f'here is your latest {post}'}
 
 
-@app.get("/posts/{id}")
+@app.get('/posts/{id}')
 async def get_post(id: int, response: Response):
     post = find_post(id)
     if not post:
-        response.status_code = status.HTTP_404_NOT_FOUND
-    return {"post_detail": f"here is your post {post}"}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Item with id: {id} not found',
+        )
+    return {'post_detail': f'here is your post {post}'}
 
 
-@app.post("/posts")
+@app.post('/posts')
 async def create_post(post: Post):
     post_dict = post.model_dump()
-    post_dict["id"] = randrange(0, 10000000)
+    post_dict['id'] = randrange(0, 10000000)
     my_post.append(post_dict)
-    return {"data": post_dict}
+    return {'data': post_dict}
